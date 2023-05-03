da1bc38ad8aa30388f56c8403122101b72f42661
Hüseyin Bartu kara <huseyinbartukara@gmail.com>
2023-05-03 22:51:49 +0300

    versiyon1

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender.currentTitle)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+              sender.alpha = 1.0
+          }
+        
+        let buttonTag = sender.currentTitle
+        playSound(buttonTag: buttonTag!)
+    }
     
-
+    func playSound(buttonTag: String) {
+        let url = Bundle.main.url(forResource: buttonTag, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
